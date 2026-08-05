f63cd6e85b8c8db064af8f8714237cbe53a4ed4a
Ruslan <ruslan220v@bk.ru>
2026-08-05 21:28:09 +0300

    test: add forbidden response checks for common user

--- tests/api/test_movies_negative.py
+++ tests/api/test_movies_negative.py
@@ -47,17 +47,32 @@ def test_common_user_cannot_create_movie(common_user):
 
 
 def test_common_user_cannot_delete_movie(super_admin, common_user):
-
+    # создаем фильм от имени супер админа
     movie_data = DataGenerator.generate_movie_data()
 
-    response = super_admin.api.movies_api.create_movie(movie_data)
-    assert response.status_code == 201
+    create_response = super_admin.api.movies_api.create_movie(movie_data)
 
-    movie_id = response.json()["id"]
+    movie_id = create_response.json()["id"]
 
-    common_user.api.movies_api.delete_movie(
+    # user пытается удалить фильм
+    delete_response = common_user.api.movies_api.delete_movie(
         movie_id
     )
+
+    assert delete_response.status_code == 403
+
+    body = delete_response.json()
+
+    assert delete_response.status_code == 403
+    assert body["message"] == "Forbidden resource"
+    assert body["error"] == "Forbidden"
+    assert body["statusCode"] == 403
+
+    # проверяем, что фильм не удалился
+    get_response = super_admin.api.movies_api.get_movie(movie_id)
+
+    assert get_response.status_code == 200
+    assert get_response.json()["id"] == movie_id
 
 def test_delete_movie_without_token(super_admin, user_session):
 
